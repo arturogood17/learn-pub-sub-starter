f0d5e6ee08ac40631fd522d407fa80089aff31d0
arturogood17 <arturo_good17@hotmail.com>
2025-11-23 00:30:01 +0100

    added: Conexión con RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conStr)
+	if err != nil {
+		log.Fatal("Error al establecer la conexión")
+	}
+	defer conn.Close()
+	fmt.Println("Conexión establecida con RabbitMQ exitosamente")
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	_, ok := <-sigs
+	if ok {
+		fmt.Println()
+		fmt.Println("Cerrando el programa")
+	}
 }
