524b160c462a86e7cb49da5fbed026821bf885db
arturogood17 <arturo_good17@hotmail.com>
2025-11-25 23:40:59 +0100

    feature: Added DeclareAndBind
    - Function that creates a channel and creates and binds a queue

--- internal/pubsub/ServerClient.go
+++ internal/pubsub/ServerClient.go
@@ -7,6 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	Transient SimpleQueueType = iota
+	Durable
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	byteVal, err := json.Marshal(val)
 	if err != nil {
@@ -19,4 +26,28 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		return err
 	}
 	return nil
+}
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	var b bool
+	if queueType == Durable {
+		b = true
+	}
+
+	q, err := ch.QueueDeclare(queueName, b, !b, !b, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	if err = ch.QueueBind(queueName, key, exchange, false, nil); err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+
 }
