36d35efb29bbee567c3e4833b108dc7093f55db8
arturogood17 <arturo_good17@hotmail.com>
2025-11-25 01:09:06 +0100

    added: Channel to to publish messages
    - Created a ampq channel to publish messages.
    - Used pubsub function to publish message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,6 +18,16 @@ func main() {
 	if err != nil {
 		log.Fatal("Error al establecer la conexión")
 	}
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Error al crear el canal")
+	}
+
+	if err = pubsub.PublishJSON(ch, string(routing.ExchangePerilDirect),
+		string(routing.PauseKey), routing.PlayingState{IsPaused: true}); err != nil {
+		log.Fatal("Error al intentar enviar el mensaje")
+	}
+
 	defer conn.Close()
 	fmt.Println("Conexión establecida con RabbitMQ exitosamente")
 	sigs := make(chan os.Signal, 1)
