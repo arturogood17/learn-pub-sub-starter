10b75db3ed0fbb85a804b2236e2744634f60fc6c
arturogood17 <arturo_good17@hotmail.com>
2025-11-25 23:40:04 +0100

    added: Added all the logic to create the Client
    - Created a connection
    - Ask username
    - Used pubsub.Function to create a channel and a queue
    - Used signal and syscall to exit gracefully

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	conStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conStr)
+	if err != nil {
+		log.Fatal("Error al establecer la conexión")
+	}
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal("Error al establecer la conexión")
+	}
+
+	queueName := fmt.Sprintf("%v.%v", routing.PauseKey, username)
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatal("Error al establecer la conexión")
+	}
+
+	defer conn.Close()
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	_, ok := <-sigs
+	if ok {
+		fmt.Println()
+		fmt.Println("Cerrando el programa")
+	}
 }
