ebf35ff733677442753ffc1d4e77783333a7969e
arturogood17 <arturo_good17@hotmail.com>
2025-12-05 19:56:04 +0100

    feature: Handler for move

--- cmd/client/clientFunc.go
+++ cmd/client/clientFunc.go
@@ -7,9 +7,16 @@ import (
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
-func HandlerCreator(gameState *gamelogic.GameState) func(routing.PlayingState) {
+func HandlerPause(gameState *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Println("> ")
 		gameState.HandlePause(ps)
+	}
+}
+
+func HandlerMove(gameState *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Println("> ")
+		gameState.HandleMove(move)
 	}
 }
