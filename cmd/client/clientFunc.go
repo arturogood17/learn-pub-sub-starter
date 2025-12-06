c7f65c9eda5c262b58939cd4ee16462a9270b905
arturogood17 <arturo_good17@hotmail.com>
2025-12-07 00:49:41 +0100

    fix: Change handler functions
    - Now the handlers return a Ack or Nack (acknowledge or negative acknowledge)

--- cmd/client/clientFunc.go
+++ cmd/client/clientFunc.go
@@ -4,19 +4,31 @@ import (
 	"fmt"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
-func HandlerPause(gameState *gamelogic.GameState) func(routing.PlayingState) {
-	return func(ps routing.PlayingState) {
-		defer fmt.Println("> ")
+func HandlerPause(gameState *gamelogic.GameState) func(routing.PlayingState) pubsub.AckType {
+	return func(ps routing.PlayingState) pubsub.AckType {
+		defer fmt.Printf("> ")
 		gameState.HandlePause(ps)
+		return pubsub.Ack
 	}
 }
 
-func HandlerMove(gameState *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(move gamelogic.ArmyMove) {
-		defer fmt.Println("> ")
-		gameState.HandleMove(move)
+func HandlerMove(gameState *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+	return func(move gamelogic.ArmyMove) pubsub.AckType {
+		defer fmt.Printf("> ")
+		resultMove := gameState.HandleMove(move)
+		switch resultMove {
+		case gamelogic.MoveOutComeSafe:
+			fallthrough
+		case gamelogic.MoveOutcomeMakeWar:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			fallthrough
+		default:
+			return pubsub.NackDiscard
+		}
 	}
 }
